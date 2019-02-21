b7d4881f66bea0787239275c249406d04033e8b5
Ruben E. Acosta <eldragondera1@gmail.com>
2019-02-21 00:25:53 -0500

    pagina home

--- src/components/App/Home.jsx
+++ src/components/App/Home.jsx
@@ -1,16 +1,64 @@
-import React from 'react'
+import React , { Component } from 'react'
 import { Redirect } from 'react-router-dom'
 import { connect } from 'react-redux'
+import Select from 'react-select'
+import logo from '../../images/logo.jpg'
+import { options } from './options'
 import actions from '../../actions'
 
+import {Container, Content, ContentKey, ContentSelect, Label, Logo,
+  Keywords, Button, Form} from '../../styles'
 
 
-function Home () {
-  return (
-    <div>
-      hola
-    </div>
-  )
+class Home extends Component {
+  constructor(props) {
+    super(props)
+    this.keyWords = ''  
+    this.state = {
+      selectedOption: null,
+    }
+  }
+
+  handleKeywords = (e) => this.keyWords = e.target.value
+  handleChange = (selectedOption) => this.setState({ selectedOption })
+
+  handleSearch = () => {
+    const keyWords = this.keyWords
+    const typeMaterial = this.state.selectedOption.value
+    console.log(keyWords, typeMaterial)
+  }
+
+  render() {
+    const { selectedOption } = this.state
+
+    return (
+      <Container>
+        <Logo src={logo} alt="logo"/>
+
+        <Form>
+          <ContentKey>
+            <Label htmlFor="keyWords">Keywords</Label>
+            <Keywords
+              style={{height: '38px'}}
+              type="text" id="keyWords" 
+              onChange={this.handleKeywords} />
+          </ContentKey>
+          <ContentSelect>
+            <Label htmlFor="select">Keywords</Label>
+            <Select
+              id="select"
+              value={selectedOption}
+              onChange={this.handleChange}
+              options={options}
+            />
+          </ContentSelect>
+          <Content>
+            <Button type='button' onClick={this.handleSearch}>Search</Button>
+          </Content>
+        </Form>
+      </Container>
+    )
+  }
 }
 
 
--- src/styles/index.js
+++ src/styles/index.js
@@ -1,2 +1,66 @@
 import styled from 'styled-components'
 // font-size: ${props => props.task ? '30px' : '50px'};
+
+
+export const Container = styled.div `
+  display: flex;
+  align-items: center;
+  width: 100vw;
+  height: 100vh;
+  flex-direction: column;
+`
+
+
+export const Logo = styled.img `
+  object-fit: contain;
+  height: 35vh;
+  margin-top: 10vh;
+`
+
+
+export const Form = styled.form `
+  border: 2px solid black;
+  background-color: lightgray;
+  display: flex;
+  align-items: center;
+  width: 70vw;
+  margin: 10px auto;
+`
+
+
+export const Content = styled.div `
+  display: flex;
+  flex-direction: column;
+  padding: 17px;
+  width: 20%;
+`
+
+export const ContentKey = styled(Content) `
+  width: 50%;  
+`
+
+export const Keywords = styled.input `
+  height: 38px;
+  border: 1px solid hsl(0,0%,80%);
+  border-radius: 4px;
+  padding: 5px;
+`
+
+export const ContentSelect = styled(Content) `
+  width: 30%;
+`
+
+export const Label = styled.label `
+  margin-bottom: 5px;
+`
+
+
+export const Button = styled.button `
+  margin-top: 29px;
+  border-radius: 4px;
+  height: 38px;
+  background-color: whitesmoke;
+`
+
+
+
